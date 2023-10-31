dc7cf65179fd3c44420f3b70d0f1fa7b2ebff9cd
DwiRayyan <149278385+DwiRayyan@users.noreply.github.com>
2023-10-31 16:34:51 +0700

    Add files via upload

---
+++ Rayyan161_2.m
@@ -0,0 +1,23 @@
+clc 
+clear 
+
+% NAMA : M Dwi Rayyan
+% NIM : 112022161
+
+% SOAL 4.2 
+
+% soal a. matrik indentitas dengan dimensi 5x5
+a = eye (5,5)
+
+% soal b. matrik dengan dimensi 3x3 dengan elemen semuanya bernilai nol
+b = zeros (3,3)
+
+% soal c. matrik dengan dimensi 4x4 dengan elemen semuanya bernilai satu
+c = ones (4,4)
+
+% soal d. matrik dengan dimensi 3x4 dimana elemen terdistribusi anatara 1 dan 0 
+% (default n=m)
+d = rand (3,4) 
+
+% soal e. matrik dengan dimensi 3x4 dimana elemen terdistribusi normal 
+e = randn (3,4)
